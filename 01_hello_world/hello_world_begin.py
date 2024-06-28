fade07cca7df7e08a118c1516d73d86427329473
Nick Sanft <nsanft@sanft.com>
2024-06-28 13:03:47 -0500

    Added more info to the first objective

--- 01_hello_world/hello_world_begin.py
+++ 01_hello_world/hello_world_begin.py
@@ -1,15 +1,23 @@
-# This is a comment! Any line starting with a # means Python will ignore it
-
 """
 This is also a comment if you put triple quotes, you can add a big block of text!
 
-Objective: create a variable called "message" before the print line and replace the "hello!" with your new variable.
+Objective: create a variable called "message" before the print function line and replace the "hello!" with your new variable.
 
 The syntax is easy!
 
 message = "YourTextGoesInHere"
 
-Then, you can just replace everything in the parenthesis in the print line.
+Then, you can just replace everything in the parenthesis in the print function line.
+
+Additional info:
+
+To start, we will learn two major parts of Python, variables and functions.
+
+Variables are a place to store values, like a number, a character, or a string.
+A function is a block of code that is called by calling its name followed by parenthesis, like print.
+We're going to use Python's built-in functions for now and will talk about how to make your own in a future step.
 """
+
+# This is a comment! Any line starting with a # means Python will ignore it
 
 print("hello!")
