a1019733ef5b6967e8d247651cda0b8cbcd265ca
Nick Sanft <nsanft@sanft.com>
2024-06-28 14:57:14 -0500

    Added step 6

---
+++ Tutorial_Steps/06_modules/modules_begin.py
@@ -0,0 +1,18 @@
+"""
+Objective: Print the local time on your computer using the time module
+
+There are some built-in functions in Python like type() and print(), but what if we want to use something else?
+
+This is where modules come in!
+
+Some are already installed by default, like time, you can use it like the below.
+
+time has a function called time_ns(), which returns the current time in nanoseconds.
+
+This is the time for the most important lesson - Google to finish the objective.
+If you can do that, you can do just about anything. I'm not even kidding.
+"""
+
+import time
+
+print(time.time_ns())
---
+++ Tutorial_Steps/06_modules/modules_end.py
@@ -0,0 +1,10 @@
+import time
+
+print(time.localtime())
+
+"""
+You may have found your own way to get the local time, and that's okay!
+There are about a million ways to do this. 
+
+...but what if you want to use something outside of the standard libraries?
+"""
