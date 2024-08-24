df333913807f21471c113555248e63f549d8d86e
Nick Sanft <nsanft@sanft.com>
2024-08-24 15:19:38 -0500

    More info to first module

--- Tutorial_Steps/01_hello_world/hello_world_begin.py
+++ Tutorial_Steps/01_hello_world/hello_world_begin.py
@@ -1,7 +1,9 @@
-"""
-This is also a comment if you put triple quotes, you can add a big block of text!
+# This is a comment! Any line starting with a # means Python will ignore it.
 
-Objective: create a variable called "message" before the print function line and replace the "hello!" with your new variable.
+"""
+This is also a comment if you put two sets of triple quotes, you can add a big block of text!
+
+Objective: create a variable called "message" before the print line and replace the "hello!" with your new variable.
 
 The syntax is easy!
 
@@ -14,10 +16,13 @@ Additional info:
 To start, we will learn two major parts of Python, variables and functions.
 
 Variables are a place to store values, like a number, a character, or a string.
+
 A function is a block of code that is called by calling its name followed by parenthesis, like print.
+In the parenthesis, you can pass different variables into it, which are called "arguments".
+In this module, we're going to use the function print, in which we are going to pass in a string.
+Python will then print whatever that is to our terminal when you click the green "run" button on the top right.
+
 We're going to use Python's built-in functions for now and will talk about how to make your own in a future step.
 """
-
-# This is a comment! Any line starting with a # means Python will ignore it
 
 print("hello!")
